72793150f0554987a01630e90361e574b71f044a
Julián González Calderón <gonzalezcalderonjulian@gmail.com>
2024-01-19 14:43:49 -0300

    recursive structure for translation

--- index.ts
+++ index.ts
@@ -1,12 +1,11 @@
 import { unified } from "unified"
 import { translatePage } from "./translate";
 import remarkStringify from "remark-stringify"
-import type { Root } from "mdast";
 
 require("dotenv").config();
 
 let pageRoot = await translatePage(process.env.PAGE_ID!);
 console.log(pageRoot)
 
-let pageString = unified().use(remarkStringify).stringify(pageRoot as Root)
+let pageString = unified().use(remarkStringify).stringify(pageRoot)
 console.log(pageString)
--- translate.ts
+++ translate.ts
@@ -1,10 +1,48 @@
 import { Client } from "@notionhq/client";
+import type { GetBlockResponse } from "@notionhq/client/build/src/api-endpoints";
+import type { Node, Root } from "mdast";
 import builder from "mdast-builder"
 
 const notionClient = new Client({
     auth: process.env.NOTION_API_KEY,
 });
 
-export async function translatePage(pageId: string) {
-    return builder.root()
+export async function translatePage(pageId: string): Promise<Root> {
+    const children = await translateChildren(pageId)
+
+    // @ts-ignore
+    return builder.root(children)
+}
+
+async function translateChildren(blockId: string): Promise<Node[]> {
+    const children = await getChildren(blockId);
+
+    return await Promise.all(children.map(async (blockResponse) => {
+        const blockNode = await translateBlock(blockResponse)
+        return blockNode
+    }));
+}
+
+async function getChildren(blockId: string): Promise<GetBlockResponse[]> {
+    let children = [];
+
+    let has_more
+    let next_cursor = undefined
+    do {
+        const response = await notionClient.blocks.children.list({
+            block_id: blockId,
+            start_cursor: next_cursor,
+        });
+
+        children.push(...response.results);
+
+        has_more = response.has_more
+        next_cursor = response.next_cursor || undefined
+    } while (has_more)
+
+    return children
+}
+
+async function translateBlock(blockResponse: GetBlockResponse): Promise<Node> {
+    return builder.paragraph()
 }
