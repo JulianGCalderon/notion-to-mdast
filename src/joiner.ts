b9b3bfa9f1dd48c4ff6d8a152f09b66ee49817d0
Julián González Calderón <gonzalezcalderonjulian@gmail.com>
2024-01-22 10:37:32 -0300

    Added support for list items

--- src/joiner.ts
+++ src/joiner.ts
@@ -1,5 +1,5 @@
-import type { Node, Root } from "mdast";
-import { visit } from "unist-util-visit";
+import type { List, ListItem, Parent, Root } from "mdast";
+import { CONTINUE, visit } from "unist-util-visit";
 
 export default function remarkListJoin() {
     return function(tree: Root) {
@@ -7,6 +7,25 @@ export default function remarkListJoin() {
     }
 }
 
-function joinerVisitor(node: Node, index?: number, parent?: Node) {
-    console.error(node, index)
+function joinerVisitor(node: List, index?: number, parent?: Parent) {
+    if (!parent || !index) {
+        return CONTINUE
+    }
+
+    let toDelete = 0
+    while (shouldBeJoined(node, index + toDelete + 1, parent)) {
+        toDelete += 1
+    }
+
+    const deletedNodes = parent.children.splice(index + 1, toDelete) as Array<List>
+    const deletedItems = deletedNodes.flatMap((list) => list.children) as Array<ListItem>
+
+    node.children.push(...deletedItems)
+
+    return CONTINUE
+}
+
+function shouldBeJoined(node: List, index: number, parent: Parent) {
+    const adjacentNode = parent.children[index]
+    return adjacentNode?.type == "list" && node.ordered == adjacentNode.ordered
 }
