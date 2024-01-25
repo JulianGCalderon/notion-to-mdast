12f33395c27e742c177b665286374db864a06549
Julián González Calderón <gonzalezcalderonjulian@gmail.com>
2024-01-25 09:50:56 -0300

    Updated search script

--- scripts/search.ts
+++ scripts/search.ts
@@ -1,8 +1,15 @@
 import { Client, isFullPage, iteratePaginatedAPI } from "@notionhq/client";
+import { title } from "../src/handle/property";
+import { ToMdast } from "../src";
+import { argv } from "bun";
 
 const client = new Client({ auth: process.env.NOTION_API_KEY });
+const toMdast = new ToMdast(client);
+
+const query = argv[2] || undefined;
 
 for await (const searchResponse of iteratePaginatedAPI(client.search, {
+    query: query,
     filter: {
         value: 'page',
         property: 'object'
@@ -12,5 +19,11 @@ for await (const searchResponse of iteratePaginatedAPI(client.search, {
         continue
     }
 
-    console.log(JSON.stringify(searchResponse, null, 1))
+    const summary = {
+        title: await title.call(toMdast, searchResponse.properties.title),
+        id: searchResponse.id,
+        url: searchResponse.url,
+    }
+
+    console.log(summary)
 }
